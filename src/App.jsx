37b5350f9bc1d5cd2938874ab815a6a239254cd8
JaissonB <097319@aluno.uricer.edu.br>
2022-04-09 17:07:17 -0300

    feat: Added whatsapp contact us

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.scss';
+import whatsapp from './images/whatsapp.png'
 import Header from './components/Header';
 import Main from './pages/Main';
 import Professional from "./pages/Professional";
@@ -12,6 +13,9 @@ function App() {
 			  <Route path='/' element={ <Main /> }/>
 			  <Route path='/professional' element={ <Professional /> }/>
       </Routes>
+      <a className='icon-whatsapp' href='https://web.whatsapp.com/send?phone=5554992026787' target='_blank' rel='noreferrer'>
+        <img className='img-whatsapp' src={whatsapp} alt='Icone do Whatsapp' />
+      </a>
     </Router>
   );
 }
